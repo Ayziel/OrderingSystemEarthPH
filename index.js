97cde42ec4a018b54ca08879f6e1ae727f427a72
Ayziel <90187527+Ayziel@users.noreply.github.com>
2024-11-26 09:17:49 +0800

    Update index.js
    Update mongoose server to 5002

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/earthph')
+mongoose.connect('mongodb://localhost:5002/earthph')
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
