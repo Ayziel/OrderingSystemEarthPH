4af39fb6f39173c52a16d102c9a62539552b3888
Ayziel <ayziel00000@gmail.com>
2024-12-17 05:17:48 +0800

    sw.js test

--- EarthPhFrontEndWeb/Pages/OrderForm/sw.js
+++ EarthPhFrontEndWeb/Pages/OrderForm/sw.js
@@ -1,15 +1,8 @@
 const CACHE_NAME = "order-management-cache-v1";
 const urlsToCache = [
   "./Agent-info.html",
-  "./Order-info.html",
-  "./Product-Preference.html",
   "./styles/agentInfo.css",
-  "./styles/orderInfo.css",
-  "./styles/productPreference.css",
-  "./script.js",
-  "./scripts/orderInfo.js",
   "./scripts/agentInfo.js",
-  // Add other JS and CSS files here
 ];
 
 self.addEventListener("install", (event) => {
@@ -21,22 +14,9 @@ self.addEventListener("install", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        if (response) {
-          // Return the cached response
-          return response;
-        }
-        // Otherwise, fetch from the network and update the cache
-        return fetch(event.request).then((networkResponse) => {
-          if (networkResponse && networkResponse.status === 200) {
-            caches.open(CACHE_NAME).then((cache) => {
-              cache.put(event.request, networkResponse.clone());
-            });
-          }
-          return networkResponse;
-        });
-      })
-    );
-  });
-  
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
