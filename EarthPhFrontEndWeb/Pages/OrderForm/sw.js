6b6c03c8cb45801bf8226f179fe4ef6ad064ddc5
Ayziel <ayziel00000@gmail.com>
2024-12-17 05:24:46 +0800

    sw test

--- EarthPhFrontEndWeb/Pages/OrderForm/sw.js
+++ EarthPhFrontEndWeb/Pages/OrderForm/sw.js
@@ -1,8 +1,8 @@
 const CACHE_NAME = "order-management-cache-v1";
 const urlsToCache = [
-  "/OrderForm/Agent-info.html",
-  "/OrderForm/styles/agentInfo.css",
-  "/OrderForm/scripts/agentInfo.js",
+  "Pages/OrderForm/Agent-info.html",
+  "Pages/OrderForm/styles/agentInfo.css",
+  "Pages/OrderForm/scripts/agentInfo.js",
 ];
 
 self.addEventListener("install", (event) => {
