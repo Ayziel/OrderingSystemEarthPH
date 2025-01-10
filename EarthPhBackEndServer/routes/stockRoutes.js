57d088ddf5472baaf21b6050c711e30739db5925
Ayziel <ayziel00000@gmail.com>
2025-01-10 14:20:41 +0800

    stockRoutes.js test

--- EarthPhBackEndServer/routes/stockRoutes.js
+++ EarthPhBackEndServer/routes/stockRoutes.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { getStock, createStock, updateStock } = require('../controllers/stockController');
-
+const StockController = require('../controllers/stockController');
 // Route to get all stock records
-router.get('/getStock', getStock);
+router.get('/getStock', StockController.getStock);
 
 // Route to create new stock
-router.post('/createStock', createStock);
+router.post('/createStock', StockController.createStock);
 
 // Route to update stock
-router.put('/updateStock', updateStock);
+router.put('/updateStock', StockController.updateStock);
 
 module.exports = router;
