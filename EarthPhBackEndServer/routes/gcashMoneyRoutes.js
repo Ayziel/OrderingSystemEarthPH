087a638b22fc10552ada3b97365a134bcb8b1f52
Ayziel <ayziel00000@gmail.com>
2025-01-15 01:15:13 +0800

    fix gcashRoutes

--- EarthPhBackEndServer/routes/gcashMoneyRoutes.js
+++ EarthPhBackEndServer/routes/gcashMoneyRoutes.js
@@ -7,5 +7,5 @@ router.post('/createGCash', gcashController.createGCash); // Create new GCash re
 router.get('/getGCash/:userUid', gcashController.getGCashByUserUid); // Get GCash by userUid
 router.put('/updateGCash', gcashController.updateGCash); // Update GCash record
 router.get('/getAllGcash', gcashController.getAllGCash);
-router.put('sendGCash', gcashController.sendGCash);
+router.put('/sendGCash', gcashController.sendGCash);
 module.exports = router;
