8cbf83a994ba77d0e50ebf255c7326bf0dcb6c49
Ayziel <ayziel00000@gmail.com>
2025-02-28 21:54:28 +0800

    adjust team controller

--- EarthPhBackEndServer/routes/teamRoutes.js
+++ EarthPhBackEndServer/routes/teamRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const teamController = require('../controller/teamController');
 
 // Route to create or update the team
-router.post('/createTeam', teamController.createOrUpdateTeam);
+router.post('/createTeam', teamController.createTeam);
 
 // Route to get the team
 router.get('/getTeam', teamController.getTeam);
